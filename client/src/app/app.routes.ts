281073d61c94ef89efaad94ae47915449e09023b
Sahil Garg <me.sahil.gg@gmail.com>
2025-12-30 20:34:55 +0530

    feat: added login and register routes.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('../routes/login').then((m) => m.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('../routes/register').then((m) => m.Register),
+  },
+];
