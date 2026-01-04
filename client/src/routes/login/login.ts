39a8852f453d48bb4165e48327af0bbacb7a2b4e
Sahil Garg <me.sahil.gg@gmail.com>
2026-01-04 12:40:56 +0530

    feat: added login form validation.

--- client/src/routes/login/login.ts
+++ client/src/routes/login/login.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+  type ValidationErrors,
+} from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,6 +14,40 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'login',
   templateUrl: 'login.html',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder);
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+  getErrorMessage(controlName: string, errors: ValidationErrors | null | undefined): string {
+    if (errors) {
+      switch (Object.keys(errors)[0]) {
+        case 'required':
+          return `${controlName} is required`;
+        case 'email':
+          return `Not a valid email`;
+        default:
+          return '';
+      }
+    }
+    return '';
+  }
+  loginHandler() {
+    console.log(this.loginForm.value);
+  }
+}
