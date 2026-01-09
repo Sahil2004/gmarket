900bf1d361f8a6350d081d2a2573b70798d1d3ad
Sahil Garg <me.sahil.gg@gmail.com>
2026-01-09 12:23:23 +0530

    fix: fixed the 2 way guard not working properly.

--- client/src/guards/auth/auth-guard.ts
+++ client/src/guards/auth/auth-guard.ts
@@ -2,21 +2,22 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
-import { Location } from '@angular/common';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
   const router = inject(Router);
-  const location = inject(Location);
   const authRoutes = ['login', 'register'];
   const currentUrlInAuthRoutes = authRoutes.some((r) => route.url.toString().includes(r));
-  if (!userService.isAuthenticated() && !currentUrlInAuthRoutes) {
-    const loginPath = '/login';
-    router.navigate([loginPath]);
-    return false;
-  }
   if (currentUrlInAuthRoutes) {
-    location.back();
+    if (userService.isAuthenticated()) {
+      router.navigate(['/']);
+      return false;
+    }
+    return true;
+  }
+  if (!userService.isAuthenticated()) {
+    router.navigate(['/login']);
+    return false;
   }
   return true;
 };
