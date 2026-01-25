4b7691a9279ec21cde576721f190ef7815cc3cd6
Sahil Garg <me.sahil.gg@gmail.com>
2026-01-25 21:33:57 +0530

    feat: added chart data get for a symbol. (#22)

--- server/controllers/market_controller.go
+++ server/controllers/market_controller.go
@@ -1,15 +1,80 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+
+	"github.com/Sahil2004/gmarket/server/dtos"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v3/client"
+)
 
 // GetSymbols godoc
 // @Summary Get Market Symbols
 // @Description GetSymbols serves the list of market symbols from a static JSON file.
-// @Tags Market
+// @Tags market
 // @Produce json
 // @Success 200 {file} file "../data/stocks.json"
 // @Failure 500 {object} dtos.ErrorDTO
 // @Router /market/symbols [get]
 func GetSymbols(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).SendFile("data/stocks.json")
+}
+
+// GetChartData godoc
+// @Summary Get Chart Data for a Symbol
+// @Description GetChartData fetches chart data for a given symbol from an external API.
+// @Tags market
+// @Accept json
+// @Produce json
+// @Param exchange query string true "Exchange (e.g., NSE)" default(NSE)
+// @Param symbol query string true "Stock Symbol (e.g., RELIANCE)" default(RELIANCE)
+// @Param range query string true "Chart Range (e.g., 1d, 5d, 1mo)" default(1d)
+// @Param interval query string true "Data Interval (e.g., 1m, 5m, 1h)" default(1m)
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} dtos.ErrorDTO
+// @Failure 500 {object} dtos.ErrorDTO
+// @Router /market/chart [get]
+func GetChartData(c *fiber.Ctx) error {
+	exchange := c.Query("exchange")
+	symbol := c.Query("symbol")
+	chartRange := c.Query("range")
+	interval := c.Query("interval")
+
+	if exchange == "" || symbol == "" || chartRange == "" || interval == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(dtos.ErrorDTO{
+			Code:    fiber.StatusBadRequest,
+			Message: "Missing required query parameters",
+		})
+	}
+
+	var exchangeSymbol string
+	switch exchange {
+	case "NSE":
+		exchangeSymbol = symbol + ".NS"
+	default:
+		exchangeSymbol = symbol
+	}
+	url := "https://query1.finance.yahoo.com/v8/finance/chart/" + exchangeSymbol + "?range=" + chartRange + "&interval=" + interval
+
+	res, err := client.Get(url)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(dtos.ErrorDTO{
+			Code:       fiber.StatusInternalServerError,
+			Message:    "Failed to fetch chart data",
+			DevMessage: err.Error(),
+		})
+	}
+
+	var result map[string]interface{}
+
+	if err := json.Unmarshal(res.Body(), &result); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(dtos.ErrorDTO{
+			Code:       fiber.StatusInternalServerError,
+			Message:    "Failed to parse chart data",
+			DevMessage: err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(result)
 }
--- server/go.mod
+++ server/go.mod
@@ -4,18 +4,20 @@ go 1.25.5
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.10
+	github.com/gofiber/fiber/v3 v3.0.0-rc.3
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/google/uuid v1.6.0
 	github.com/jaevor/go-nanoid v1.4.0
 	github.com/lib/pq v1.10.9
 	github.com/swaggo/fiber-swagger v1.3.0
 	github.com/swaggo/swag v1.16.6
-	golang.org/x/crypto v0.43.0
+	golang.org/x/crypto v0.44.0
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
 	github.com/go-openapi/jsonpointer v0.22.4 // indirect
 	github.com/go-openapi/jsonreference v0.21.4 // indirect
 	github.com/go-openapi/spec v0.22.3 // indirect
@@ -26,20 +28,25 @@ require (
 	github.com/go-openapi/swag/stringutils v0.25.4 // indirect
 	github.com/go-openapi/swag/typeutils v0.25.4 // indirect
 	github.com/go-openapi/swag/yamlutils v0.25.4 // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/gofiber/schema v1.6.0 // indirect
+	github.com/gofiber/utils/v2 v2.0.0-rc.2 // indirect
+	github.com/klauspost/compress v1.18.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/tinylib/msgp v1.5.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.65.0 // indirect
+	github.com/valyala/fasthttp v1.68.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/mod v0.29.0 // indirect
-	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
 	golang.org/x/tools v0.38.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
