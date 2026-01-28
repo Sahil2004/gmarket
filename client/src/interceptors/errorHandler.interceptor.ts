8b5a36ee62ed20e4466bfa8f0676ab12e3c12d26
Sahil Garg <me.sahil.gg@gmail.com>
2026-01-28 11:48:46 +0530

    fix: fixed the interceptor.

--- client/src/interceptors/errorHandler.interceptor.ts
+++ client/src/interceptors/errorHandler.interceptor.ts
@@ -1,26 +1,33 @@
-import { HttpEvent, HttpEventType, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+} from '@angular/common/http';
 import { inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable, tap } from 'rxjs';
+import { catchError, EMPTY, Observable, throwError } from 'rxjs';
 import { IError } from '../types';
 
 export function errorHandlerInterceptor(
   req: HttpRequest<any>,
   next: HttpHandlerFn,
 ): Observable<HttpEvent<unknown>> {
+  let _snackBar = inject(MatSnackBar);
   return next(req).pipe(
-    tap((event) => {
-      if (event.type === HttpEventType.Response) {
-        if (event.status >= 400 && event.status < 500) {
-          let _snackBar = inject(MatSnackBar);
-          let snackBarRef = _snackBar.open((event.body as IError).message, 'Close', {
-            duration: 3000,
-          });
-          snackBarRef.onAction().subscribe(() => {
-            snackBarRef.dismiss();
-          });
-        }
+    catchError((err: HttpErrorResponse) => {
+      if (err.status >= 400 && err.status <= 500) {
+        let snackBarRef = _snackBar.open((err.error as IError).message, 'Close', {
+          duration: 3000,
+        });
+        snackBarRef.onAction().subscribe(() => {
+          snackBarRef.dismiss();
+        });
+        return EMPTY;
       }
+      console.error('An unexpected error occurred:', err);
+      return throwError(() => err);
     }),
   );
 }
