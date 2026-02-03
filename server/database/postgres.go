04505d65fc55b5a4671c00d7fecb103e2d3be14f
Sahil Garg <me.sahil.gg@gmail.com>
2026-02-03 10:41:33 +0530

    feat: added db pool config.

--- server/database/postgres.go
+++ server/database/postgres.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/Sahil2004/gmarket/server/config"
 	_ "github.com/lib/pq"
@@ -17,6 +18,12 @@ func PostgresDBConnection() (*sql.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Error: Not connected to the database. %w", err)
 	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(time.Hour)
+	db.SetConnMaxIdleTime(15 * time.Minute)
+
 	if err := db.Ping(); err != nil {
 		defer db.Close()
 		return nil, fmt.Errorf("Error: Unable to ping the database. %w", err)
